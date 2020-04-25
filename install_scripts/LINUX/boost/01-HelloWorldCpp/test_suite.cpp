64ba756115c2e4c0ca00bfad8928333b193d3429
MenosGrandes <menosg@tutanota.com>
2020-04-25 22:41:47 +0200

    Added boost installation for CPP and Python.

---
+++ install_scripts/LINUX/boost/01-HelloWorldCpp/test_suite.cpp
@@ -0,0 +1,10 @@
+#include <boost/variant.hpp>
+#include <string>
+#include <iostream>
+int main()
+{
+    boost::variant<double, char, std::string> v;
+    v = 3.14;
+    v = 'A';
+    v = "Boost";
+}
--- install_scripts/LINUX/boost/install.sh
+++ install_scripts/LINUX/boost/install.sh
@@ -1,10 +1,15 @@
 wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
 tar -zxvf boost_1_72_0.tar.gz
 cd boost_1_72_0/
+#Python version
 ./bootstrap.sh install --with-libraries=all --with-toolset=gcc --with-python=/usr/bin/python3.8
 sudo ./b2 -a -q install --with=all -j$(nproc) --with-python --debug-configuration
+#Cpp version
+./bootstrap.sh install --with-libraries=all --with-toolset=gcc
+sudo ./b2 -a -q install --with=all -j$(nproc) --debug-configuration
 sudo ldconfig
 cd ..
+#Make Python TEST
 mkdir build
 cd build
 cmake ..
