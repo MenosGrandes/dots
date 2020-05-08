fda4e75c4f17319aa44bf6148f7937ef59aeb96c
MenosGrandes <menosg@tutanota.com>
2020-05-08 09:07:43 +0200

    Changed installation of choco in Windows

---
+++ install_scripts/WINDOWS/installOtherPackages.bat
@@ -0,0 +1,2 @@
+choco install miktex texstudio jabref deluge conemu google-backup-and-sync firefox-dev --pre windirstat curl  strawberryperl python3 packages.config -y
+set PATH=%PATH%;C:\Python38
--- install_scripts/WINDOWS/setup_vim.bat
+++ install_scripts/WINDOWS/setup_vim.bat
@@ -1,3 +1,2 @@
-choco install neovim -y
 COPY ..\..\vim\init.vim %localappdata%\nvim
 nvim-qt +PlugUpdate
