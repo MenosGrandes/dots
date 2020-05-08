2ea1816cbdc84829bf8d726ae7d017084a7395e0
MenosGrandes <menosg@tutanota.com>
2020-05-08 09:08:52 +0200

    apt-get insted of apt in nvim installation'

--- install_scripts/LINUX/nvim.sh
+++ install_scripts/LINUX/nvim.sh
@@ -1,5 +1,6 @@
-apt purge vi* --assume-yes
-apt install build-essential gcc git cmake automake libtool-bin libtool ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip --assume-yes
+#!/bin/bash
+apt-get purge vi* --assume-yes
+apt-get install build-essential gcc git cmake automake libtool-bin libtool ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip --assume-yes
 
 git clone https://github.com/neovim/neovim.git
 cd neovim/
