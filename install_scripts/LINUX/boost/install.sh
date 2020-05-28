2cab6deafe938ad86c6fb346c054a3c4f9d27d2d
MenosGrandes <menosg@tutanota.com>
2020-05-28 21:52:07 +0200

    Instalation of boost must have take into considaration python version. Added

--- install_scripts/LINUX/boost/install.sh
+++ install_scripts/LINUX/boost/install.sh
@@ -1,8 +1,12 @@
+# python2-dev must be installed
+# find which version will be called using python --version
+
 wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
 tar -zxvf boost_1_72_0.tar.gz
 cd boost_1_72_0/
 #Python version
-./bootstrap.sh install --with-libraries=all --with-toolset=gcc --with-python=/usr/bin/python3.8
+sudo ln -f /usr/bin/python3 /usr/bin/python
+./bootstrap.sh install --with-libraries=all --with-toolset=gcc --with-python=/usr/bin/python3
 sudo ./b2 -a -q install --with=all -j$(nproc) --with-python --debug-configuration
 #Cpp version
 ./bootstrap.sh install --with-libraries=all --with-toolset=gcc
