587803c70ddf9909b30036d15ff52613239678cb
mg <menosg@tutanota.com>
2020-06-05 18:10:45 +0200

    Full installation of neovim

--- install_scripts/LINUX/nvim.sh
+++ install_scripts/LINUX/nvim.sh
@@ -1,10 +1,60 @@
 #!/bin/bash
+
+PACKAGES="\
+	git \
+	curl \
+	python3-dev \
+	python3-pip \
+	wget \
+	gcc  \
+	git  \
+	cmake  \
+	automake  \
+	libtool-bin  \
+	libtool  \
+	ninja-build  \
+	gettext  \
+	libtool  \
+	libtool-bin  \
+	autoconf  \
+	automake  \
+	cmake  \
+	g++  \
+	pkg-config  \
+	unzip  \
+"
+
+PYTHON_PACKAGES="\
+	numpy \
+	matplotlib \
+	neovim \
+	scipy\
+	pandas\
+	sympy\
+	nose\
+	flake8 \
+	autopep8 \
+	jedi \
+	scipy \
+	"
+
 apt-get purge vim* --assume-yes
-apt-get install build-essential gcc git cmake automake libtool-bin libtool ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip --assume-yes
+apt-get install $PACKAGES --assume-yes
 
 git clone https://github.com/neovim/neovim.git
 cd neovim/
 git checkout stable
 make CMAKE_BUILD_TYPE=Release
 make install
-
+curl -sL https://deb.nodesource.com/setup_13.x | bash - && apt-get install -y -qq nodejs && npm install -g neovim 
+python3 -m pip install --user $PYTHON_PACKAGES
+mkdir -p ~/.config/nvim 
+mv  ~/dots/vim/init.vim ~/.config/nvim/
+mv  ~/dots/vim/coc-settings.json ~/.config/nvim/
+nvim +PlugInstall +qall
+mkdir -p /root/.config/coc/extensions
+cd /root/.config/coc/extensions
+echo '{"dependencies":{}}'> package.json
+npm install coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+nvim +CocUpdateSync +qall
+rm -rf neovim
