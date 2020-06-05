6f9db4794fcacbf5b75a3bc10d22906ddd172506
mg <menosg@tutanota.com>
2020-06-05 16:32:27 +0200

    NVIM installation proper whildcard removal of vi*

--- install_scripts/LINUX/nvim.sh
+++ install_scripts/LINUX/nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get purge vi* --assume-yes
+apt-get purge vim* --assume-yes
 apt-get install build-essential gcc git cmake automake libtool-bin libtool ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip --assume-yes
 
 git clone https://github.com/neovim/neovim.git
