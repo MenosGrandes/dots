a9c5c29537e3001a065975d075b464156b738228
MenosGrandes <menosg@tutanota.com>
2020-10-08 12:37:32 +0200

    Added tmux configuration and minor changes in vim

--- install_scripts/LINUX/nvim.sh
+++ install_scripts/LINUX/nvim.sh
@@ -40,23 +40,22 @@ PYTHON_PACKAGES="\
 
 TEMP_USER=$1
 USER_HOME=/home/$TEMP_USER
-mkdir -p $USER_HOME/.config/nvim 
+mkdir -p $USER_HOME/.config/nvim
 
-apt-get -qq purge vim* --assume-yes 
+apt-get -qq purge vim* --assume-yes
 apt-get -qq install $PACKAGES --assume-yes
 
 git clone -q https://github.com/neovim/neovim.git $USER_HOME/dots
 cd neovim/
 git checkout stable
-make -s CMAKE_BUILD_TYPE=Release 
+make -s CMAKE_BUILD_TYPE=Release
 make -s install
-curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get install -y -qq nodejs && npm install -g neovim 
-python3 -m pip install --user -q $PYTHON_PACKAGES 
-mv  $USER_HOME/dots/vim/init.vim $USER_HOME/.config/nvim/
-mv  $USER_HOME/dots/vim/coc-settings.json $USER_HOME/.config/nvim/
+curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get install -y -qq nodejs && npm install -g neovim
+python3 -m pip install --user -q $PYTHON_PACKAGES
+mv -t $USER_HOME/.config/nvim/ $USER_HOME/dots/vim/*
 nvim +PlugInstall +qall
-mkdir -p /root/.config/coc/extensions
-cd /root/.config/coc/extensions
+mkdir -p /root/.config/coc/extensions # why is it root?...
+cd /root/.config/coc/extensions #same as above
 echo '{"dependencies":{}}'> package.json
 npm install coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 nvim +CocUpdateSync +qall
---
+++ tmux/install_tmux.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/.tmux.conf #TODO move to .conf and set it as tmux dot file location
