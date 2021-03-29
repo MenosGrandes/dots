b226ebc94222fcf3b2c16ed61e622f2e6928cb75
MenosGrandes <menosgrandes@tutanota.com>
2021-03-29 10:42:04 +0200

    Updated ubuntu setup

--- install_scripts/LINUX/python/virtualenvwrapper_config.sh
+++ install_scripts/LINUX/python/virtualenvwrapper_config.sh
@@ -1,4 +1,4 @@
-python3 -m pip install --update pip setuptools wheel
+python3 -m pip install --upgrade pip setuptools wheel
 python3 -m pip install --user virtualenvwrapper
 
 cp .virtualenvwrapper.conf ~/.config/
--- install_scripts/LINUX/setupUbuntu.sh
+++ install_scripts/LINUX/setupUbuntu.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
+set -x
+sudo apt update -y -qq
+sudo apt upgrade -y -qq
 sudo apt install build-essential ninja-build cmake git kdiff3 tmux fzf ripgrep -qq -y
 bash ./nvim.sh $USER
-bash git/install_aliases.sh
-bash tmux/install_tmux.sh
+pushd $(pwd)
+cd git
+bash install_aliases.sh
+cd ../tmux
+bash install_tmux.sh
+cd ../python
 bash python/virtualenvwrapper_config.sh
-
+popd
