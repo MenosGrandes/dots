65e6013933aa3cdae21447cdc188cc2b2ce7ecd0
mg <mg@localhost.localdomain>
2024-12-21 14:37:45 +0100

    Remove pupilLabs and GO/CCLS installation

--- install_scripts/LINUX/installCCLS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt install -qq -y zlib1g-dev libncurses-dev clang libclang-dev ccls clang-format gdb > /dev/null
-
--- install_scripts/LINUX/installGO.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update -qq
-sudo apt install golang-go -qq -y
-go install github.com/mattn/efm-langserver@latest
-echo 'export GOPATH="/home/$USER/go/bin" ' >> ~/.bashrc
-echo 'export PATH="$PATH:$GOPATH" ' >> ~/.bashrc
-source /home/$USER/.bashrc
